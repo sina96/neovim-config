4ee5a4674f44f00eb0c3da00af9d243c91c2d317
sina96 <sina@sinabastani.dev>
2026-04-05 13:34:57 +0200

    added nvim tmux nav

--- lua/sina96/init.lua
+++ lua/sina96/init.lua
@@ -8,4 +8,10 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 -- vim.opt.relativenumber = true
 
+-- -- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
 require("sina96.lazy_init")
